2447d4a0c93761d2f7badf87e5958df84a3d0f76
David Morales <david.morales@mawesi.net>
2025-06-10 00:50:35 -0500

    Funcion en javascripr reverse string

---
+++ template/script.js
@@ -0,0 +1,8 @@
+const revertirCadena = (cadena) => {
+  return cadena.split('').reverse().join('');
+};
+
+// Ejemplos de uso:
+console.log(revertirCadena('agente')); // Salida: etnega
+console.log(revertirCadena('JavaScript')); // Salida: tpircSavaJ
+console.log(revertirCadena('Hola Mundo')); // Salida: odnuM aloH
